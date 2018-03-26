05402667e70de5cbbfe96ca536c153ad9b9f480e
YongPilMoon <myp7@urbanbase.com>
2018-03-27 02:41:47 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+      console.log("logo svg" + logo);
+    return (
+      <div className="App">
+        <header className="App-header">
+            <svg>
+                <use xlinkHref={`${logo}#test`}/>
+            </svg>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
