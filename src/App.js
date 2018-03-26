919e532227e67c873c01d1ca5fd832819087dc52
YongPilMoon <myp7@urbanbase.com>
2018-03-27 02:52:42 +0900

    Add post-receive test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
             <svg>
                 <use xlinkHref={`${logo}#test`}/>
             </svg>
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React test</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
