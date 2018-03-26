2601397a750ed7202e53dbcad04e6ad3e22c7ae4
YongPilMoon <myp7@urbanbase.com>
2018-03-27 03:29:33 +0900

    post receive test5

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
             <svg>
                 <use xlinkHref={`${logo}#test`}/>
             </svg>
-          <h1 className="App-title">Welcome to React test4</h1>
+          <h1 className="App-title">Welcome to React test5</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
